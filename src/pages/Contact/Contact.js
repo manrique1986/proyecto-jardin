ee4d86c1bf058fb0d093ba044c205fedd4b55c9a
Leandro Martini <leandromartini86@gmail.com>
2022-07-16 12:42:21 -0300

    contacto-garden

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,7 +6,9 @@ import React from "react";
 export default function Contact() {
   return (
     <div action="Form.php" method="post">
+     
       <form  className="formulario">
+      <h1 className="tituloContac">Contacto</h1>
         <input type="text" name="nombre" placeholder="Nombre"></input>
         <br />
         <input type="text" name="email" placeholder="Mail"></input>
@@ -18,11 +20,11 @@ export default function Contact() {
           placeholder="Mensaje"
         ></textarea>
         <button className="botonForm">Enviar</button>
+      </form>
 
-        <div className="imgForm">
+      <div className="imgForm">
           <img src={imagenForm} alt={"imageForm"} />
         </div>
-      </form>
     </div>
   );
 }
